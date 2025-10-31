4c9950b6d242c8ee8a214365894f3076fe5e086d
sbh2025dev <sbh2025dev@gmail.com>
2025-10-31 15:33:48 +0000

    red color for snake

--- script.js
+++ script.js
@@ -30,14 +30,7 @@ const MAP_ZOOM = 17;
 
 // Color palette for snake
 const SNAKE_COLORS = [
-  "#667eea",
-  "#764ba2",
-  "#f093fb",
-  "#4facfe",
-  "#00f2fe",
-  "#43e97b",
-  "#fa709a",
-  "#fee140",
+  "#ff0000", // bright red
 ];
 let currentSnakeColor = SNAKE_COLORS[0];
 
